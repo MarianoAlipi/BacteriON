7eeeaa653aab4307a16573ee0474df515fb8f6d3
Roberta González <a01570010@itesm.mx>
2019-05-06 10:46:04 -0500

    modifica elicitadores para todos los niveles

--- bacterion/Elicitador.java
+++ bacterion/Elicitador.java
@@ -27,6 +27,9 @@ public class Elicitador extends Item{
     public Elicitador(Game game, int x, int y, int width, int height, int speed, int direction) {
         super(game, x, y, width, height, speed);
         dir = direction;
+        if (game.getLevel() == 2) {
+            this.speed = (int) (Math.random() * 10 +1);
+        } 
     }
     
     public boolean isExploded(){
@@ -38,6 +41,22 @@ public class Elicitador extends Item{
      */
     @Override
     public void tick() {
+        //1 = de arriba a abajo, 2 = de abajo a arriba, 3 = de izq a der, 4 = de der a izq
+        if (game.getLevel() == 3) {
+            if (dir == 1) {
+                setY(y+speed);
+                setX(x+(int)(Math.random() * 20 - 5));
+            } else if (dir == 2) {
+                setY(y-speed);
+                setX(x+(int)(Math.random() * 20 - 5));
+            } else if (dir == 3) {
+                setX(x+speed);
+                setY(y+(int)(Math.random() * 20 - 15));
+            } else if (dir == 4) {
+                setX(x-speed);
+                setY(y+(int)(Math.random() * 20 - 15));
+            }
+        } 
         if (dir == 1) {
             setY(y+speed);
         } else if (dir == 2) {
@@ -47,6 +66,7 @@ public class Elicitador extends Item{
         } else if (dir == 4) {
             setX(x-speed);
         }
+        
     }
     
     public void explode(){
