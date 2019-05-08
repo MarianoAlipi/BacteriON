908cc7a45281d5880b2e6bdca883430a38e40eb9
Roberta González <a01570010@itesm.mx>
2019-05-07 19:34:46 -0500

    borra clase que no se usaba, javadoc de mousemanager

--- pure_engine/MouseManager.java
+++ pure_engine/MouseManager.java
@@ -20,49 +20,78 @@ public class MouseManager  implements MouseListener, MouseMotionListener {
     private int x;                      // to get x position of the mouse
     private int y;                      // to get y position of the mouse
     
+    /**
+     * Constructor method for mouse manager
+     */
     public MouseManager() {
         izquierdo = false;
         derecho = false;
     }
-
+    
+    /**
+     * gets x value for mouse position
+     * @return an <code>int</code> corresponding to the x position
+     */
     public int getX() {
         return x;
     }
 
+    /**
+     * gets y value for mouse position
+     * @return an <code>int</code> corresponding to the y position
+     */
     public int getY() {
         return y;
     }
   
+    /**
+     * gets the perimeter for a 10*10 rectangle around the mouse
+     * @return the <code>rectangle</code> surrounding the mouse
+     */
     public Rectangle getPerimeter() {
         return new Rectangle(x, y, 10, 10);
     }
-  
+    
+    /**
+     * gets x y values for mouse position
+     * @return the <code>Point</code> corresponding to the position
+     */
     public Point getPoint() {
         return new Point(x,y);
     }
-
+    
+    /**
+     * sets the left click value
+     * @param izq a <code>boolean</code> that says if clicked or not
+     */
     public void setIzquierdo(boolean izq) {
         this.izquierdo = izq;
     }
     
+    /**
+     * checks if the left click is pressed
+     * @return a <code>boolean</code> that says if it's pressed
+     */
     public boolean isIzquierdo() {
         return izquierdo;
-        
     }
-
+    /**
+     * checks if the right click is pressed
+     * @return a <code>boolean</code> that says if it's pressed
+     */
     public boolean isDerecho() {
         return derecho;
     }
     
     @Override
     public void mouseClicked(MouseEvent e) {
-        /*if (e.getButton() == MouseEvent.BUTTON1) {
-            izquierdo = true;         
-        } else if (e.getButton() == MouseEvent.BUTTON3) {
-            derecho = true;        
-        }*/
+        
     }
 
+    /**
+     * checks if the a button is pressed
+     * @param e a <code>MouseEvent</code> with the action
+     */
     @Override
     public void mousePressed(MouseEvent e) { 
         if (e.getButton() == MouseEvent.BUTTON1) {
@@ -73,6 +102,10 @@ public class MouseManager  implements MouseListener, MouseMotionListener {
         }
     }
 
+    /**
+     * checks if the a button is released
+     * @param e a <code>MouseEvent</code> with the action
+     */
     @Override
     public void mouseReleased(MouseEvent e) {
         if (e.getButton() == MouseEvent.BUTTON1) {
@@ -95,6 +128,10 @@ public class MouseManager  implements MouseListener, MouseMotionListener {
     public void mouseDragged(MouseEvent e) {
     }
 
+    /**
+     * checks if the mouse is moved
+     * @param e a <code>MouseEvent</code> with the action
+     */
     @Override
     public void mouseMoved(MouseEvent e) {
         x = e.getX();
